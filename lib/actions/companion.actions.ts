be4e7a366cea7c72a74b66604306a5d774b000cc
Anshul-04 <anshulonmars@gmail.com>
2025-06-08 04:35:40 +0530

    Implement Profile Page / My Journey

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -108,3 +108,16 @@ export const getUserSessions = async (userId: string, limit = 10) => {
 
   return data.map(({ companions }) => companions);
 };
+
+// This function retrieves all companions created by a specific user.
+export const getUserCompanions = async (userId: string) => {
+  const supabase = createSupabaseClient();
+  const { data, error } = await supabase
+      .from('companions')
+      .select()
+      .eq('author', userId)
+
+  if(error) throw new Error(error.message);
+
+  return data;
+}
